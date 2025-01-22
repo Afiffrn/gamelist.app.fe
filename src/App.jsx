e4e03eeaf0a2db4599c4d89c0c534e4a106c9704
Afif Frizkianto <144239654+Afiffrn@users.noreply.github.com>
2025-01-22 23:52:42 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GameList from "./components/GameList"; // Ensure this is imported
 import UserList from "./components/UserList"; // Ensure this is imported
 import Login from "./components/Login"; // Ensure this is imported
-import Dashboard from "./components/Dashboard"; // Ensure this is imported
+// import Dashboard from "./components/Dashboard"; // Ensure this is imported
 import DeveloperList from "./components/DeveloperList";
 import GenreList from "./components/GenreList"; // Ensure this is imported
 import PublisherList from "./components/PublisherList"; // Ensure this is imported
