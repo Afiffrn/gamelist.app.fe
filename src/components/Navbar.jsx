602c680026e779c468db5e1b58734198335a4408
Afif Frizkianto <144239654+Afiffrn@users.noreply.github.com>
2025-01-23 09:47:56 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ handleLogout }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <nav className="py-2 px-2 bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 flex justify-between items-center">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">My GameList</h1>
-        <div>
-          <Link to="/games" className="mr-4 hover:underline">
-            Game List
-          </Link>
-          <Link to="/users" className="mr-4 hover:underline">
-            User List
-          </Link>
+        <h1 className="text-2xl font-bold text-blue-600 text-shadow">My GameList</h1>
+        <div className="relative">
           <button
-            className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            onClick={handleLogout} // Use the handleLogout function from props
+            onClick={toggleDropdown}
+            className="mr-4 bg-blue-600 text-white font-bold py-2 px-4 rounded transition-transform hover:transform hover:-translate-y-1"
           >
-            Logout
+            Menu
           </button>
+          {isDropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10">
+              <Link to="/games" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                Game List
+              </Link>
+              <Link to="/users" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                User List
+              </Link>
+              <Link to="/developers" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                Developer List
+              </Link>
+              <Link to="/genres" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                Genre List
+              </Link>
+              <Link to="/publishers" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                Publisher List
+              </Link>
+              <Link to="/ratings" className="block px-4 py-2 text-gray-200 hover:bg-gray-700">
+                Rating List
+              </Link>
+              <button
+                className="block w-full text-center px-4 py-2 text-red-500 hover:bg-gray-700"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </nav>
